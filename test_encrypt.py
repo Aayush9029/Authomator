8a84bb3fd35f0245cead2d389737c18924820385
Aayush9029 <aayushpokharel36@gmail.com>
2021-11-17 19:05:57 -0500

    pylint

--- test_encrypt.py
+++ test_encrypt.py
@@ -1,49 +1,25 @@
-from Helper.EncryptorDecryptor import *
-from Utils.TestModules import *
+'''
+Test file for EncryptorDecryptor
+'''
+from random import randint
+
 import pytest
 
-# Manual random tests
-def test_encrypt_decrypt1():
-    key = generate_random_code()
-    source = generate_random_string(100)
-    encrypted = encrypt(key, source)
-    decrypted = decrypt(key, encrypted)
-    assert source == decrypted
-        
-def test_encrypt_decrypt2():
-    key =  generate_random_code()
-    source = generate_random_string(0)
-    encrypted = encrypt(key, source)
-    decrypted = decrypt(key, encrypted)
-    assert source == decrypted
-    
+from helper.encryptor_decryptor import decrypt, encrypt
+from utils.test_modules import generate_random_code, generate_random_string
 
-# Manual tests
-def test_encrypt_decrypt3():
-    key = b"0000"
-    source = b"Hello World!"
-    encrypted = encrypt(key, source)
-    decrypted = decrypt(key, encrypted)
-    assert source == decrypted
-
-
-def test_encrypt_decrypt4():
-    key = b"4914239klada/23'aasd/asdasd//asdasdasd"
-    source = b"Sint velit do in eiusmod eiusmod consectetur est do excepteur mollit culpa esse ullamco cupidatat."
-    encrypted = encrypt(key, source)
-    decrypted = decrypt(key, encrypted)
-    assert source == decrypted
-
-def test_encrypt_decrypt5(message: bytes = b'lorem ipsum', key: bytes = b'lorem ipsum'):
-    encrypted = encrypt(key, message)
-    decrypted = decrypt(key, encrypted)
-    assert message == decrypted
 
 # Random tests for messages
 messages = [generate_random_string(randint(100,10000)) for _ in range(100)]
 
-@pytest.mark.parametrize("test_input,expected", zip(messages, messages))
-def test_encrypt_decryptRandom(test_input, expected):
+@pytest.mark.parametrize("test_input, _", zip(messages, messages))
+def test_encrypt_decrypt_random(test_input, _):
+    '''
+    Test encrypt and decrypt for random messages using the same key
+    Asserts that the encrypted message is equal to the original message
+    Parameters:
+        message (bytes): The message to be encrypted
+    '''
     key = generate_random_code()
     encrypted = encrypt(key, test_input)
     decrypted = decrypt(key, encrypted)
@@ -52,8 +28,14 @@ def test_encrypt_decryptRandom(test_input, expected):
 # Random tests for keys
 keys = [generate_random_code(randint(0,100000)) for _ in range(100)]
 
-@pytest.mark.parametrize("test_input,expected", zip(keys, keys))
-def test_encrypt_decryptRandomKey(test_input, expected):
+@pytest.mark.parametrize("test_input, _", zip(keys, keys))
+def test_encrypt_decrypt_random_key(test_input, _):
+    '''
+    Test encrypt and decrypt for random keys using the same message
+    Asserts that the encrypted message is equal to the original message
+    Parameters:
+        key (bytes): The key to be used for encryption
+    '''
     source = generate_random_string(100)
     encrypted = encrypt(test_input, source)
     decrypted = decrypt(test_input, encrypted)
@@ -63,8 +45,15 @@ def test_encrypt_decryptRandomKey(test_input, expected):
 messages = [generate_random_string(randint(100,10000)) for _ in range(100)]
 keys = [generate_random_code(randint(0,100000)) for _ in range(100)]
 
-@pytest.mark.parametrize("test_input,expected", zip(messages, keys))
-def test_encrypt_decryptRandomKeyAndMessage(test_input, expected):
+@pytest.mark.parametrize("test_input,_", zip(messages, keys))
+def test_encrypt_decrypt_random_message(test_input, _):
+    '''
+    Test encrypt and decrypt for random keys and messages
+    Asserts that the encrypted message is equal to the original message
+    Parameters:
+        message (bytes): The message to be encrypted
+        key (bytes): The key to be used for encryption
+    '''
     encrypted = encrypt(test_input, test_input)
     decrypted = decrypt(test_input, encrypted)
     assert test_input == decrypted
